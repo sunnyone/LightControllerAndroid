0de34f22786a20f3d947d4bce069b08abaadbd21
Yoichi Imai <sunnyone41@gmail.com>
2013-09-16 20:29:33 +0900

    Added PrefActivity.

---
+++ src/org/sunnyone/lightcontroller/PrefActivity.java
@@ -0,0 +1,14 @@
+package org.sunnyone.lightcontroller;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class PrefActivity extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref);
+	}
+
+}
