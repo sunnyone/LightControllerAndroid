c6bbf4ea15fa4bd846f462a1f876a1fc5752b554
Yoichi Imai <sunnyone41@gmail.com>
2013-09-16 18:46:01 +0900

    Added listeners to buttons.

--- src/org/sunnyone/lightcontroller/MainActivity.java
+++ src/org/sunnyone/lightcontroller/MainActivity.java
@@ -3,6 +3,9 @@ package org.sunnyone.lightcontroller;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,31 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button buttonOn = (Button)findViewById(R.id.button_on);
+		buttonOn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "Light is on", Toast.LENGTH_LONG).show();
+			}
+		});
+
+		Button buttonOff = (Button)findViewById(R.id.button_off);
+		buttonOff.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "Light is off", Toast.LENGTH_LONG).show();
+			}
+		});
+
+		Button buttonClose = (Button)findViewById(R.id.button_close);
+		buttonClose.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				MainActivity.this.finish();
+			}
+		});
+
 	}
 
 	@Override
